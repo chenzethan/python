56f644d741c16caf0c58b05f007ef563e84f34a3
chenzethan <67557098+chenzethan@users.noreply.github.com>
2021-07-10 21:50:55 -0400

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,9 @@
+character_name = "Ethan"
+character_age = "15 years old"
+print("Hi my name is " + character_name +"")
+print("I am " + character_age +"")
+
+character_name = "Natalie"
+character_age = "9 years old"
+print("I have a sister named " + character_name+ "")
+print("she is " + character_age + "")
